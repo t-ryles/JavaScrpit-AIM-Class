265959783a10d284f5c19067f0907b57c49c13ab
t-ryles <tajhakeem.ryles@gmail.com>
2024-06-06 22:12:27 -0500

    Homework file

---
+++ homeWork-6.10.24.js
@@ -0,0 +1,24 @@
+// Write a function that, when passed a certain number of inches, will print in the console the equivalent number of centimeters.
+// An inch is 2.54 centimeters.
+
+function inchConverter(inches) {
+  return inches / 2.54
+}
+
+print(inchConverter(3))
+
+// Write a function that determines if a passed in number is positive or negative and even or odd.
+
+// Write a function that calculates the ticket price for a passed in age with the following conditions: age below 12 pay a ticket price of 5, age below 18 pay a ticket price of 10, age below 60 pay a ticket price of 20, age over 60 pay a ticket price of 15.
+
+// Write a function that determines and categorizes the body mass index (BMI) of an adult using pounds and inches. 
+// The formula for BMI using these is weight/height * height * 703. The categories are: BMI below 18.5 is Underweight; BMI between 18.5 and 24.9 is Healthy Weight. BMI between 25.0 and 29.9 is Overweight. BMI of 30.0 and above is Obese.
+
+// Write a function to sort 3 numbers from highest to lowest and print the sorted numbers to the console.
+
+// Write a function that console.logs the sum of two arrays (the sum of all their elements). The arrays will only include integer numbers.
+
+// Write a function to check if a string passed into the function contains the letter ‘s’. Print yes to the console if it does and no if it does not.
+
+// Write a function that finds the summation of every number from 1 to a number passed into the function. 
+// The number passed in will always be a positive number greater than 0.
