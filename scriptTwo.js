d081ec36158caf73d03d5b43162cadc3b71245a8
t-ryles <tajhakeem.ryles@gmail.com>
2024-06-24 20:04:43 -0500

    goToDinner

--- scriptTwo.js
+++ scriptTwo.js
@@ -1,2 +1,35 @@
 console.log("New Notes:");
 
+const goToDinner = () => {
+
+  const menu = [
+    {
+      dishName: "Beef Terderloin",
+      price: 14.75
+    },
+    {
+      dishName: "Ribeye",
+      price: 17.50
+    },
+    {
+      dishName: "BBQ Chicken",
+      price: 13.25
+    }
+  ];
+  let menuList = "";
+  for (let i = 0; i < menu.length; i++) {
+    menuList += (`${i+1}. is ${menu[i].dishName}: $${menu[i].price.toFixed(2)} \n`)
+  }
+
+  let dinnerChoice = prompt(`Please select your choice of meal:\n${menuList}`)
+
+  while(Number.isNaN(dinnerChoice) || dinnerChoice <= 0 || dinnerChoice > menu.length ) {
+    console.log(`That is a invalid choice, try again.`);
+    dinnerChoice = parseInt(prompt(`
+      Please select your choice of meal:\n${menuList}`
+    ));
+  }
+ console.log(`You have selected the ${menu[dinnerChoice - 1].dishName}.`);
+};
+
+goToDinner();
