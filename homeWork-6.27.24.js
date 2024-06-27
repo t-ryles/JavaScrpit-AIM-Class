afc8969173f8f44b96fffcbb8ab11ebeacf7b352
t-ryles <tajhakeem.ryles@gmail.com>
2024-06-27 18:52:04 -0500

    Create homework js page

---
+++ homeWork-6.27.24.js
@@ -0,0 +1,28 @@
+//! Due July 1
+
+
+// Write a function that will multiply four numerical arguments together, with the following conditions:
+// If the first argument is not present, its value should be 2
+// If the second argument is not present, its value should be 3
+// If the third argument is not present, its value should be the sum of the first argument and the second argument
+// If the fourth argument is not present, its value should be the sum of the second argument and the third argument
+// console.log the result of the multiplication
+// You will call the function five times using the following arguments: 3, 5, 6, 4
+// For the first call, use all four arguments
+// For the second call, use the first three arguments
+// For the third call, use the first two arguments
+// For the fourth call, use the first argument alone
+// For the fifth call, use the last three arguments
+// Hint: you need to look up how you will make the first argument not present so that its value is 2
+
+// Write a recursive function to compute the sum of an array of integers
+
+// Using a closure, write a function called maxPlusMin that does the following:
+// Takes an array of integers
+// Finds the maximum and minimum values from the array  
+// Adds the maximum and minimum values together
+// Multiplies the sum of the maximum and minimum by a value to be provided from outside the function
+// Test the function using the following information:
+
+// const numbers = [5, 2, 6, 9, 7];
+// console.log(maxPlusMinMultiplied(2));  // should be 22
