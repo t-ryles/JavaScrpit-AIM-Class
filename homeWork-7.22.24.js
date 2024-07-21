4e65d659da22a241efba9cd367b0310938ab5900
t-ryles <tajhakeem.ryles@gmail.com>
2024-07-21 10:53:42 -0500

    Try, catch, finally
    Trying problem set two

--- homeWork-7.22.24.js
+++ homeWork-7.22.24.js
@@ -1,8 +1,3 @@
-// Going back to the Actor, Player, and Enemy classes we created in class, create another subclass of Actor called Npc. 
-// Npc inherits move() and distanceTo(), and it has its own method called dialog(). 
-// When the Player is within a distance of 1 from the Npc, the Npc should console.log(“Drink this!”) and the Player should get 5 additional hps. 
-// However, the Player should not go over 100 hp.
-
 class Actor {
   constructor (startX, startY) {
     this.x = startX;
@@ -42,16 +37,20 @@ class Enemy extends Actor {
   }
 }
 
+// Going back to the Actor, Player, and Enemy classes we created in class, create another subclass of Actor called Npc. 
+// Npc inherits move() and distanceTo(), and it has its own method called dialog(). 
 class Npc extends Actor {
   constructor( startX, startY) {
     super(startX, startY)
   }
 
   distanceCheck(player) {
+    // When the Player is within a distance of 1 from the Npc, the Npc should console.log(“Drink this!”) and the Player should get 5 additional hps. 
     if ( this.distanceTo ( player ) <= 1 ) {
       if ( player.hp <= 95 ) {
         this.dialog();
-        player.hp += 5;
+        // However, the Player should not go over 100 hp.
+        player.hp = Math.min(player.hp + 5, 100);
       }
     }
   }
@@ -59,18 +58,51 @@ class Npc extends Actor {
   dialog(){
     console.log("Drink this!");
   }
-
 }
 
 // Create a subclass of Player called Mage. Mage will inherit from Actor and Player and will have its own method called teleport(). 
-// When teleport() is called, the Mage’s x and y position will be changed so they are in a different position further than they would get with the move() method.
+class Mage extends Player {
+  constructor(startX, startY, hp) {
+    super(startX, startY, hp)
+  }
+  // When teleport() is called, the Mage’s x and y position will be changed so they are in a different position further than they would get with the move() method.
+  teleport(teleportX, teleportY) {
+    this.x = teleportX;
+    this.y = teleportY;
+  }
+}
 
 
-// Create a subclass of Enemy called Orc. Orc inherits from Actor and Enemy, but it overrides the attack() method of Enemy so that an attack causes the player to lose 20 hp instead of 10. 
+// Create a subclass of Enemy called Orc. Orc inherits from Actor and Enemy
+class Orc extends Enemy {
+
+  // It overrides the attack() method of Enemy so that an attack causes the player to lose 20 hp instead of 10.
+  attack (player) {
+    if (this.distanceTo(player) < 4) {
+      player.hp -= 20;
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
 
 
-// Switching gears to try…catch…finally. Write a JavaScript function that takes a number as a parameter and throws a custom Error if the number is not an integer.
-
+// Switching gears to try…catch…finally.
+// Write a JavaScript function that takes a number as a parameter and throws a custom Error if the number is not an integer.
+function isANumber(input) {
+  try {
+    typeof input == "number" 
+    console.log(input);
+    
+    } catch (err) {
+      console.log("Error: ", err);
+  } finally {
+    console.log('Finish running function.');
+  }
+}
+let f;
+isANumber(f);
 
 // Write a JavaScript function that uses a try…catch block to catch and handle a Range Error when accessing an array with an invalid index.
 
